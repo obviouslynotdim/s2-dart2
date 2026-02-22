3acd1d2ad881afb24035efc2023da28267fd01ab
Setha Vathanak <154043526+obviouslynotdim@users.noreply.github.com>
2026-02-22 22:42:27 +0700

    Update downloads_screen.dart

--- w4_practice/2_download_app/ui/screens/downloads/downloads_screen.dart
+++ w4_practice/2_download_app/ui/screens/downloads/downloads_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../providers/theme_color_provider.dart';
 import '../../theme/theme.dart';
 import 'widgets/download_controler.dart';
-
+import 'widgets/download_tile.dart'; // tile
 
 class DownloadsScreen extends StatelessWidget {
   // Create the list of fake ressources
@@ -23,24 +23,36 @@ class DownloadsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: currentThemeColor.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text(
-            "Downloads",
-            style: AppTextStyles.heading.copyWith(
-              color: currentThemeColor.color,
-            ),
+    return ListenableBuilder(
+      listenable: themeProvider,
+      builder: (context, child) {
+        final currentThemeColor = themeProvider.themeColor;
+        return Container(
+          color: currentThemeColor.backgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Downloads",
+                style: AppTextStyles.heading.copyWith(
+                  color: currentThemeColor.color,
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              // TODO - Add the Download tiles
+              Expanded(
+                child: ListView.builder(
+                  itemCount: controllers.length,
+                  itemBuilder: (context, index) => DownloadTile(controller: controllers[index]),
+                ),
+              ),
+            ],
           ),
-
-          SizedBox(height: 50),
-
-          // TODO - Add the Download tiles
-        ],
-      ),
+        );
+      },
     );
   }
 }
