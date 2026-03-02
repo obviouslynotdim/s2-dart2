a278579d77be42aefc08b09e3f36bbe29c0b6882
Setha Vathanak <154043526+obviouslynotdim@users.noreply.github.com>
2026-03-02 17:55:42 +0700

    Refactor AppSettingsState to use repository pattern
    Refactor AppSettingsState to inject repository and load settings on initialization. Added save functionality for theme changes.

--- w6_smallhomework/lib/ui/states/settings_state.dart
+++ w6_smallhomework/lib/ui/states/settings_state.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/widgets.dart';
- 
+import '../../data/settings/app_settings_repository.dart';
 import '../../model/settings/app_settings.dart';
 
 class AppSettingsState extends ChangeNotifier {
- 
+ final AppSettingsRepository _repository; // inject repo
   AppSettings? _appSettings;
  
-  AppSettingsState();
+  AppSettingsState(this._repository) {
+    init(); // load when created
+  }
 
   Future<void> init() async {
     // Might be used to load data from repository
+    _appSettings = await _repository.load();
+    notifyListeners();
   }
 
   ThemeColor get theme => _appSettings?.themeColor ?? ThemeColor.blue;
@@ -17,6 +21,8 @@ class AppSettingsState extends ChangeNotifier {
   Future<void> changeTheme(ThemeColor themeColor) async {
     if (_appSettings == null) return;
     _appSettings = _appSettings!.copyWith(themeColor: themeColor);
+
+    await _repository.save(_appSettings!); // save the new theme to repo
 
     notifyListeners();
   }
